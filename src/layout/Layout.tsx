7fa9fedfdfecac6f55668dc0844c43acc3bcee4c
Alexander Mendivil <alexanderima@outlook.es>
2023-03-14 21:18:47 -0700

    uso de api call

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,18 +1,32 @@
+import { useEffect, useState } from "react";
+import { getVehicles } from "../apis/apis";
 import { VehicleUpperList } from "../components/VehicleUpperList"
 import { ListType } from "../enums/ListTypeEnum";
-import { Vehicle } from "../types/types";
+import { VehicleInterface } from "../types/types";
 
 // Lista de los vehiculos
-const list:Vehicle[] = [ {id: 1, name: 'cargo', status: true, usuario: 'test'}, {id: 1, name: 'cargo', status: true, usuario: 'test'}, {id: 1, name: 'cargo', status: true, usuario: 'test'} ];
 export const Layout = () => {
+
+  const [ mercadoVehicles, setMercadoVehicles ] = useState<VehicleInterface[]>();
+  const [ meteringVehicles, setMeteringVehicles ] = useState<VehicleInterface[]>();
+
+  useEffect(()=>{
+    const apiCall = async () => {
+      const data = await getVehicles();
+      setMercadoVehicles(data?.mercadoArray);
+      setMeteringVehicles(data?.meteringArray);
+    }
+    apiCall();
+  },[])
+
   return (
 
     // clase col-12, ocupa 12 columnas en la pantalla ( toda la pantalla )
     <div className='col-12 h-100'>
       {/* Lista superior */}
-            <VehicleUpperList vehicleList={list} typeList={ListType.upper} />
+            <VehicleUpperList vehicleList={mercadoVehicles} typeList={ListType.upper} />
       {/* Lista inferior */}
-            <VehicleUpperList vehicleList={list} typeList={ListType.bottom}/>
+            <VehicleUpperList vehicleList={meteringVehicles} typeList={ListType.bottom}/>
     </div>
   )
 }
