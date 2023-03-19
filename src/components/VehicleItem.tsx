ceb8f0aecae7a52657df100bc79c552f95fc0255
Alexander Mendivil <alexanderima@outlook.es>
2023-03-19 14:57:59 -0700

    imagen correcta y estado correcto

--- src/components/VehicleItem.tsx
+++ src/components/VehicleItem.tsx
@@ -1,8 +1,8 @@
 import { VehicleInterface } from '../types/types';
 import vehicleItemStyle from './Vehicle.module.css';
-import image1 from '../assets/images/upp.jpeg';
-import image2 from '../assets/images/sit.jpeg';
-import image3 from '../assets/images/down.jpeg';
+import upp from '../assets/images/upp.jpeg';
+import sit from '../assets/images/sit.jpeg';
+import down from '../assets/images/down.jpeg';
 
 
 //prop del vehiculo
@@ -10,26 +10,25 @@ interface VehicleItemsPros{
     vehicle: VehicleInterface
 }
 
-const randomImage = () =>{
-  const imageRandom = Math.round(Math.random() * 3);
-  return  imageRandom === 1 ? image1 : imageRandom === 2 ? image2 : image3;
+const getImage = (vehicleType: string) =>{
+  return vehicleType === 'Motrec' ? down : vehicleType === 'HP' ? upp : sit;
 }
 
 export const VehicleItem = ( { vehicle }: VehicleItemsPros ) => {
   return (
 
     // class para hacer una carta que ocupe 3 col con margen en el top 5, margen right de 3 y margen left en 3
-    <div className={`card col-3 mt-3 mx-3 mb-1 ${vehicleItemStyle.backgroundTransparent}`}>
+    <div className={`card col-3 mt-3 mx-3 mb-1 ${vehicleItemStyle.backgroundItem}`}>
       {/* clase para el cuerpo de la carta */}
   <div className="card-body">
 
     <h5 className="card-title text-center">{vehicle.id}</h5>
 
-        <img src={randomImage()} alt="" className='img-thumbnail rounded mx-auto d-block w-25 h-25'/>
+        <img src={getImage(vehicle.tipo_equipo)} alt="" className='img-thumbnail rounded mx-auto d-block w-25 h-25'/>
 
       <div className={` ${vehicleItemStyle.imageBottomStyle}`}>
         {/* TODO: cambiar claxon por status */}
-          <h3 className={`text-center ${vehicle.claxon ? vehicleItemStyle.activeStatus : vehicleItemStyle.inactiveStatus}`}>Status</h3>
+          <h3 className={`text-center ${vehicle.estado_equipo === 'Activo' ? vehicleItemStyle.activeStatus : vehicleItemStyle.inactiveStatus}`}>Status</h3>
           <p className="text-center">{vehicle.nombre}</p>
       </div>
 
