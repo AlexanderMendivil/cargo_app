174d2c1e809b32ee941cbcebd798318e9ba4007b
Alexander Mendivil <alexanderima@outlook.es>
2023-03-21 22:59:11 -0700

    equipo movil en item

--- src/components/VehicleItem.tsx
+++ src/components/VehicleItem.tsx
@@ -18,10 +18,11 @@ export const VehicleItem = ( { vehicle }: VehicleItemsPros ) => {
   return (
 
     // class para hacer una carta que ocupe 3 col con margen en el top 5, margen right de 3 y margen left en 3
-    <div className={`card col-3 mt-3 mx-3 mb-1 ${vehicleItemStyle.backgroundItem}`}>
+    <div className={`card col-3 mx-3 mb-1 ${vehicleItemStyle.backgroundItem} ${vehicleItemStyle.cardSize}`}>
       {/* clase para el cuerpo de la carta */}
   <div className="card-body">
 
+    <h5 className="card-title text-center">Equipo movil: {vehicle.equipomovil}</h5>
     <h5 className="card-title text-center">{vehicle.id}</h5>
 
         <img src={getImage(vehicle.tipo_equipo)} alt="" className={`img-thumbnail rounded mx-auto d-block ${vehicleItemStyle.imageSize}`}/>
